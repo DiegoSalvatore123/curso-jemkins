06b287c29f10f558c409b4e3cb42bbfe35dfd6a4
Diego Salvatore <salvatore_diego@hotmail.com>
2025-12-10 12:37:28 -0300

    aync call

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,3 @@
 echo "Hola mundo desde Jenkins y Github "
 echo "Probando desde Jenkins y Github cada 5 minutos"
-echo "trigger con webhook desde git y usando ngrok"
+echo "trigger con webhook desde git y usando ngrok async call"
