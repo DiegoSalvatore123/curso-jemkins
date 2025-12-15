31b4142dc724635b88f460a87c91848641434b92
Diego Salvatore <salvatore_diego@hotmail.com>
2025-12-15 16:55:41 -0300

    modificando permisos e instaladores

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -6,14 +6,21 @@ if [! -d "venv"]; then
     python3 -m venv venv
 fi
 
-source vend/bind/activate
+echo "Activando el entorno virtual correctamente"
+if [! -f "venv/bin/activate"]; then
+    source vend/bind/activate
+elif [ -f "venv/Script/activate"]; then #para windows
+    source vend/Script/activate
+else
+    echo "NO se pudo activar el entorno virtual."
+    exit 1
+fi
 
-echo "activando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+echo "Instando dependencias"
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "Ejecutando pruebas con Piptest y generar reportes"
 pytest tests/ --junitxml=reports/test-result.xml --html=reports/test-results.html --self-contained-html
-
 
 echo "pruebas finalizadas, resultados en reportes"
