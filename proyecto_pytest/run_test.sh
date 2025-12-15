2487eaa57d14a5401e5ce1cef1141a2aac837ed3
Diego Salvatore <salvatore_diego@hotmail.com>
2025-12-15 16:43:51 -0300

    ejecutando validacion y creacion del entorno virtual

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 echo "activando el entorno virtual"
+if [!"-d venv"]: then
+    echo "sino existe el entorno virtual, se crea"
+    python3 -m venv venv
+fi
 source vend/bind/activate
 
 echo "activando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Ejecutando pruebas con Piptest y generar reportes"
